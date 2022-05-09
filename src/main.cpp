7c07255148695d2bd1ce53bf2910f9eba4301d6c
siikanen <git@siikanen.mailer.me>
2022-05-09 16:16:49 +0300

    Add setup mode WiFi AP
    Display can setup its own AP if no WiFi is available

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,98 @@
 
 #include <M5EPD.h>
+#include <Preferences.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
+
+#include "WebServer.h"
 
 M5EPD_Canvas canvas(&M5.EPD);
 
+const IPAddress SETUP_IP_ADDR(192, 168, 69, 1);
+const char* SETUP_SSID = "BOOKING_SETUP";
+
+// Initialize HTTP Server
+WebServer webServer(80);
+
+// Config store
+Preferences preferences;
+
+String WIFI_SSID = "";
+String WIFI_PASS = "";
+
+bool restoreWifiConfig();
+void setupMode();
+String makePage(String title, String contents);
+
 void setup() {
 	M5.begin();
+
+	Serial.println(F("========== Monad Booking =========="));
+	Serial.println(F("Booting up..."));
+
+	Serial.println(F("Setting up E-ink display..."));
 	M5.EPD.SetRotation(0);
 	M5.EPD.Clear(true);
 
+	Serial.println(F("Setting up RTC..."));
 	M5.RTC.begin();
 
-	WiFi.begin("SSID", "PASSWORD");
+	Serial.println(F("Restoring WiFi-configuration..."));
 
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
+	if (restoreWifiConfig()) {
+		Serial.println(F("WiFi-config restored!"));
+		// connect
+	} else {
+		Serial.println(F("No wifi configuration stored"));
+		Serial.println(F("Entering setup-mode..."));
+		// Setupmode
+		setupMode();
 	}
-
-	// canvas.createCanvas(540, 960);
-	canvas.createCanvas(960, 540);
-	canvas.setTextSize(3);
-	canvas.fillCanvas(0xeeef);
-	canvas.drawString("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 110, 110);
-	// canvas.drawJpgUrl("https://m5stack.oss-cn-shenzhen.aliyuncs.com/image/example_pic/flower.jpg");
-	// canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
-	// canvas.createCanvas(540, 960);
-	// canvas.setTextSize(3);
-	// canvas.drawJpgUrl("https://m5stack.oss-cn-shenzhen.aliyuncs.com/image/example_pic/flower.jpg");
-	canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
-	Serial.print(canvas.readPixel(100, 100));
+	setRoutes();
+	webServer.begin();
 }
 
-void loop() {}
+bool restoreWifiConfig() {
+	preferences.begin("wifi-config");
+	delay(10);
+
+	WIFI_SSID = preferences.getString("WIFI_SSID");
+	WIFI_PASS = preferences.getString("WIFI_PASS");
+
+	return WIFI_SSID.length() > 0;
+}
+
+String makePage(String title, String contents) {
+	String s = "<!DOCTYPE html><html><head>";
+	s += "<meta name=\"viewport\" content=\"width=device-width,user-scalable=0\">";
+	s += "<title>";
+	s += title;
+	s += "</title></head><body>";
+	s += contents;
+	s += "</body></html>";
+	return s;
+}
+
+void setRoutes() {
+	webServer.on("/", []() {
+		Serial.println("Handling request for /");
+		webServer.send(200, "text/html", makePage("TEST", "<h1>Toimiiko?</h1>"));
+	});
+
+	webServer.onNotFound([]() {
+		String s = "<h1>AP mode</h1><p><a href=\"/settings\">Wi-Fi Settings</a></p>";
+		Serial.println("Not found");
+		webServer.send(200, "text/html", makePage("AP mode", s));
+	});
+}
+
+void setupMode() {
+	Serial.printf("Starting Access Point at \"%s\"\n", SETUP_SSID);
+	WiFi.disconnect();
+	delay(100);
+	WiFi.softAPConfig(SETUP_IP_ADDR, SETUP_IP_ADDR, IPAddress(255, 255, 255, 0));
+	WiFi.softAP(SETUP_SSID);
+	WiFi.mode(WIFI_MODE_AP);
+}
+
+void loop() { webServer.handleClient(); }
