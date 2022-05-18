42f31567834bf9039e4566482ce797d311ab20a1
Eero Lehtinen <eero.lehtinen@monad.fi>
2022-05-18 14:37:17 +0300

    Fix authflow script

--- scripts/oauth2flow.py
+++ scripts/oauth2flow.py
@@ -1,4 +1,7 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+import os.path
 
 TOKEN_FILE = "token.json"
 CREDENTIALS_FILE = "credentials.json"
