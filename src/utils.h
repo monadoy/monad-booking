e199c21b9e68e183342936fbe6d66fea8cf48919
Eero Lehtinen <eero.lehtinen@monad.fi>
2022-05-25 13:11:07 +0300

    Move result to utils file

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,72 @@
 #include <Arduino.h>
 
+#include <memory>
+
 namespace {
 String ssid_;
 String password_;
 }  // namespace
 
 namespace utils {
+
+/**
+ * Contains either a successful result or an error.
+ * Check first which value is contained with functions isOk() or isErr().
+ * Then you can get a shared_ptr to the value with functions ok() or err().
+ */
+template <typename T, typename E>
+struct Result {
+  private:
+	Result(std::shared_ptr<T> _ok, std::shared_ptr<E> _err) : ok_{_ok}, err_{_err} {}
+
+	const std::shared_ptr<T> ok_ = nullptr;
+	const std::shared_ptr<E> err_ = nullptr;
+
+  public:
+	Result(const Result& other) = default;
+	Result(Result&& other) = default;
+	Result& operator=(const Result& other) = default;
+	Result& operator=(Result&& other) = default;
+	virtual ~Result() = default;
+
+	static Result makeOk(T* value) {
+		if (!value)
+			throw std::runtime_error("Tried to make an ok result with a null pointer");
+		return Result(std::shared_ptr<T>(value), nullptr);
+	}
+
+	static Result makeOk(std::shared_ptr<T> value) {
+		if (!value)
+			throw std::runtime_error("Tried to make an ok result with a null pointer");
+		return Result(value, nullptr);
+	}
+
+	static Result makeErr(E* error) {
+		if (!error)
+			throw std::runtime_error("Tried to make an error result with a null pointer");
+		return Result(nullptr, std::shared_ptr<E>(error));
+	}
+
+	static Result makeErr(std::shared_ptr<E> error) {
+		if (!error)
+			throw std::runtime_error("Tried to make an error result with a null pointer");
+		return Result(nullptr, error);
+	}
+
+	bool isOk() const { return ok_ != nullptr; }
+	bool isErr() const { return err_ != nullptr; }
+
+	std::shared_ptr<T> ok() const {
+		assert(isOk());
+		return ok_;
+	}
+
+	std::shared_ptr<E> err() const {
+		assert(isErr());
+		return err_;
+	}
+};
+
 void connectWiFi(const String& ssid, const String& password);
 
 void ensureWiFi();
