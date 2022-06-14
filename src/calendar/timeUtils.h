e772c506c4fbbf4bcba742b2be02abf2e43a3898
Eero Lehtinen <eero.lehtinen@monad.fi>
2022-06-14 11:01:13 +0300

    Keep documentation of a moved function

--- src/calendar/timeUtils.h
+++ src/calendar/timeUtils.h
@@ -7,6 +7,17 @@ namespace timeutils {
 
 time_t getNextMidnight(time_t now);
 
+/**
+ * Parses an RFC 3339 time string into a UTC time_t,
+ * Not a full RFC 3339 parser, but common (and used by google) formats shown below are supported.
+ * 2022-12-30T23:59:59Z
+ * 2022-12-30T23:59:59+03:00
+ * 2022-12-30T23:59:59-03:00
+ * 2022-12-30T23:59:59.000Z
+ * 2022-12-30T23:59:59.000+03:00
+ * 2022-12-30T23:59:59.000-03:00
+ * Seconds may contain any number of digits after the decimal point.
+ */
 time_t parseRfcTimestamp(const String& input);
 
 }  // namespace timeutils
