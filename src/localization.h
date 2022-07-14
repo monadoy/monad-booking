a1dfdc61ad7f9cefd525c3eacaace1a347e6945b
Eero Lehtinen <eero.lehtinen@monad.fi>
2022-07-14 11:00:55 +0300

    Robustify adding new localization messages
    Removes the need to manually keep two arrays in sync. Now we have a
    single source of truth.

--- src/localization.h
+++ src/localization.h
@@ -6,46 +6,34 @@
 
 #include "utils.h"
 
-enum class L10nMessage {
-	BOOK,
-	FREE,
-	CANCEL,
-	UNTIL_NEXT,
-	FREE_ROOM,
-	BOOK_ROOM,
-	NEXT_EVENT,
-	NO_UPCOMING_EVENTS,
-	BOOKED,
-	NOT_BOOKED,
-	BOOK_ROOM_QUESTION,
-	RELEASE_QUESTION,
-	SETTINGS,
-	VERSION,
-	NEW_EVENT_SUMMARY,
-	UPDATE,
-	LATEST_VERSION,
-	SIZE
-};
+// Add message names here, they must have corresponding keys in localization.json
+#define L10N_MESSAGES(F)  \
+	F(BOOK)               \
+	F(FREE)               \
+	F(CANCEL)             \
+	F(UNTIL_NEXT)         \
+	F(FREE_ROOM)          \
+	F(BOOK_ROOM)          \
+	F(NEXT_EVENT)         \
+	F(NO_UPCOMING_EVENTS) \
+	F(BOOKED)             \
+	F(NOT_BOOKED)         \
+	F(BOOK_ROOM_QUESTION) \
+	F(RELEASE_QUESTION)   \
+	F(SETTINGS)           \
+	F(VERSION)            \
+	F(NEW_EVENT_SUMMARY)  \
+	F(UPDATE)             \
+	F(LATEST_VERSION)
 
-using L10nMessageArray = std::array<String, (size_t)L10nMessage::SIZE>;
+#define L10N_MESSAGE_AS_ENUM(M) M,
+#define L10N_MESSAGE_AS_STRING(M) #M,
 
-const L10nMessageArray messageNames{"BOOK",
-                                    "FREE",
-                                    "CANCEL",
-                                    "UNTIL_NEXT",
-                                    "FREE_ROOM",
-                                    "BOOK_ROOM",
-                                    "NEXT_EVENT",
-                                    "NO_UPCOMING_EVENTS",
-                                    "BOOKED",
-                                    "NOT_BOOKED",
-                                    "BOOK_ROOM_QUESTION",
-                                    "RELEASE_QUESTION",
-                                    "SETTINGS",
-                                    "VERSION",
-                                    "NEW_EVENT_SUMMARY",
-									"LATEST_VERSION",
-									"UPDATE"};
+enum class L10nMessage { L10N_MESSAGES(L10N_MESSAGE_AS_ENUM) SIZE };
+
+using L10nStringArray = std::array<String, (size_t)L10nMessage::SIZE>;
+
+const L10nStringArray messageNames{L10N_MESSAGES(L10N_MESSAGE_AS_STRING)};
 
 class Localization {
   public:
@@ -56,7 +44,7 @@ class Localization {
 	std::unique_ptr<utils::Error> _readMessages(const String& lang);
 
 	String _currentLang = "";
-	L10nMessageArray _messages{};
+	L10nStringArray _messages{};
 };
 
 #endif
