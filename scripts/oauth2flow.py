6c8f92360be2c093d5d7df6cca30266c04bdecc7
Eero Lehtinen <eero.lehtinen@monad.fi>
2022-10-21 12:30:33 +0300

    Fix variable name clash

--- scripts/oauth2flow.py
+++ scripts/oauth2flow.py
@@ -10,10 +10,10 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 
 def main():
     flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-    token = flow.run_local_server(port=0)
+    credentials = flow.run_local_server(port=0)
 
     with open(TOKEN_FILE, "w") as token:
-        token.write(token.to_json())
+        token.write(credentials.to_json())
 
 
 if __name__ == "__main__":
